4f7142af88ee8bc006f9b0abaa8bb6fce3b35c88
CharlesLuxinger <charlesluxinger@hotmail.com>
2019-07-06 09:49:54 -0300

    117. Stack Trace

--- Udemy/Java/course28/src/application/Program.java
+++ Udemy/Java/course28/src/application/Program.java
@@ -6,24 +6,37 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
+		method1();		
+		System.out.println("End of Program");
+	}
+	
+	public static void method1() {
+		System.out.println("***#1. Start***");
+		method2();
+		System.out.println("***#1. End***");
+	}
 
+	public static void method2() {
+		System.out.println("***#2. Start***");
 		Scanner sc = new Scanner(System.in);
-
 		try {
+			
 			String[] vect = sc.nextLine().split(" ");
 			int position = sc.nextInt();
 			System.out.println(vect[position]);
+			
 		} catch (ArrayIndexOutOfBoundsException aib) {
+			
 			System.out.println("Invalid Position!");
-
+			aib.printStackTrace();
+			
 		} catch (InputMismatchException im) {
+			
 			System.out.println("Invalid Input Error!");
+			im.printStackTrace();
 		}
-		
-		System.out.println("End of Program");
-		
+		System.out.println("***#2. End***");
 		sc.close();
-
 	}
-
+	
 }
