d7e22173eec67a9998ec83bcbb228f57aa58cac3
CharlesLuxinger <charlesluxinger@hotmail.com>
2019-07-06 18:06:41 -0300

    120. Bad solutions to Handle Exceptions in Java

--- Udemy/Java/course30/src/model/entities/Reservation.java
+++ Udemy/Java/course30/src/model/entities/Reservation.java
@@ -39,15 +39,24 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Error in reservation: Reservation dates must be future dates!!!";
+		}
+		if (!checkOut.after(checkIn)) {
+			return "Error in reservation: Check-out date must be after check-in date!!!";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+
+		return null;
 	}
 
 	@Override
 	public String toString() {
-		return "Reservation: Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut)
-				+ ", duration: " + duration() + " nights" ;
+		return "Reservation: Room " + roomNumber + ", check-in: " + sdf.format(checkIn) + ", check-out: "
+				+ sdf.format(checkOut) + ", duration: " + duration() + " nights";
 	}
 
 }
