9acc05a1b8cf8847be89abd1a18b3f907b41b6ac
CharlesLuxinger <charlesluxinger@hotmail.com>
2019-07-11 23:27:33 -0300

    161. Show Folders and Files

---
+++ Udemy/Java/course36/src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a folder path: ");
+		String strPath = sc.nextLine();
+
+		File path = new File(strPath);
+
+		// isDirectory lista somente o que for diretório ou pasta
+		File[] folders = path.listFiles(File::isDirectory);
+
+		System.out.println("Folders: ");
+		for (File folder : folders) {
+			System.out.println(folder);
+		}
+
+		// isFile lista somente o que for arquivos
+		File[] files = path.listFiles(File::isFile);
+
+		System.out.println("Files: ");
+		for (File file : files) {
+			System.out.println(file);
+		}
+		
+		// Criar sub-pasta
+		boolean success = new File(strPath + "\\New folder").mkdir();
+
+		sc.close();
+	}
+
+}
