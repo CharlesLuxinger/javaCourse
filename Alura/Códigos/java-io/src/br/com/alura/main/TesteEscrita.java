870f171bfc38d4dabffb0cfd7096bcb7eb1de09a
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-07 00:08:46 -0300

    7.2.1 - File Write with java.io

---
+++ Alura/Códigos/java-io/src/br/com/alura/main/TesteEscrita.java
@@ -0,0 +1,25 @@
+package br.com.alura.main;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class TesteEscrita {
+
+	public static void main(String[] args) throws IOException {
+		// Output File
+		OutputStream file = new FileOutputStream("C:\\temp\\out.txt");
+		Writer input = new OutputStreamWriter(file);
+		BufferedWriter bw = new BufferedWriter(input);
+
+		bw.write("1. Teste saída!!!!");
+		bw.newLine();
+		bw.write("2. Teste saída!!!!");
+
+		bw.close();
+	}
+
+}
