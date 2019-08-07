7f597e2e5e1981af3670600b7113b74021385dc4
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-07 20:56:51 -0300

    7.3.3 - Printer X Stream

--- Alura/Códigos/java-io/src/br/com/alura/main/TesteEscrita.java
+++ Alura/Códigos/java-io/src/br/com/alura/main/TesteEscrita.java
@@ -2,9 +2,12 @@ package br.com.alura.main;
 
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.io.Writer;
 
 public class TesteEscrita {
@@ -18,8 +21,25 @@ public class TesteEscrita {
 		bw.write("1. Teste saída!!!!");
 		bw.newLine();
 		bw.write("2. Teste saída!!!!");
-
 		bw.close();
+
+		BufferedWriter bw2 = new BufferedWriter(new FileWriter("C:\\temp\\out2.txt"));
+		bw2.write("1. Teste saída!!!!");
+		bw.newLine();
+		bw2.write("2. Teste saída!!!!");
+		bw2.close();
+
+		PrintStream ps = new PrintStream("C:\\temp\\out3.txt");
+		ps.println("1. Teste saída!!!!");
+		ps.println();
+		ps.println("1. Teste saída!!!!");
+		ps.close();
+
+		PrintWriter pw = new PrintWriter("C:\\temp\\out4.txt");
+		pw.println("1. Teste saída!!!!");
+		pw.println();
+		pw.println("1. Teste saída!!!!");
+		pw.close();
 	}
 
 }
