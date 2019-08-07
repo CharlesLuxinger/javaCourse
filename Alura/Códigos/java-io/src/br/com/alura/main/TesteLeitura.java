835d5c3fff6949049970b3b686b70c34f521d3d5
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-07 00:00:36 -0300

    7.1.4 - File read with java.io

--- Alura/Códigos/java-io/src/br/com/alura/main/TesteLeitura.java
+++ Alura/Códigos/java-io/src/br/com/alura/main/TesteLeitura.java
@@ -3,19 +3,24 @@ package br.com.alura.main;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class TesteLeitura {
 
 	public static void main(String[] args) throws IOException {
 		// Input File
-		FileInputStream file = new FileInputStream("C:\\temp\\lorem.txt");
-		InputStreamReader input = new InputStreamReader(file);
+		InputStream file = new FileInputStream("C:\\temp\\lorem.txt");
+		Reader input = new InputStreamReader(file);
 		BufferedReader br = new BufferedReader(input);
 
 		String line = br.readLine();
 
-		System.out.println(line);
+		while (line != null) {
+			System.out.println(line);
+			line = br.readLine();
+		}
 
 		br.close();
 	}
