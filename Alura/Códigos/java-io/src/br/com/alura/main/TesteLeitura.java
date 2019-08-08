46f85b7a4de95e25b0b7910f65863882fbd03deb
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-07 22:17:44 -0300

    7.4 - Leitura com Scanner e Formatação de Valores

--- Alura/Códigos/java-io/src/br/com/alura/main/TesteLeitura.java
+++ Alura/Códigos/java-io/src/br/com/alura/main/TesteLeitura.java
@@ -1,11 +1,14 @@
 package br.com.alura.main;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class TesteLeitura {
 
@@ -21,8 +24,29 @@ public class TesteLeitura {
 			System.out.println(line);
 			line = br.readLine();
 		}
-
 		br.close();
+
+		Scanner sc = new Scanner(new File("C:\\temp\\test.csv"));
+		while (sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+
+			sc.useLocale(Locale.US);
+			sc.useDelimiter(",");
+			String field1 = sc.next();
+			int field2 = sc.nextInt();
+			double field3 = sc.nextDouble();
+
+			//Preenche com espaço
+			String fmt1 = String.format("%10s", field1);
+			//Preenche com zero
+			String fmt2 = String.format("%08d", field2);
+			String fmt3 = String.format(new Locale("pt", "BR"), "%05.2f", field3);
+
+			String[] fields = sc.nextLine().split(",");
+			System.out.println(fields);
+		}
+		sc.close();
+
 	}
 
 }
