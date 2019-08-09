a71b09a7bc5c665d457c2df195bb9dff082a1200
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-08 23:40:56 -0300

    8.8 - Outros sets e iterators

--- Alura/Códigos/java-collections/src/br/com/alura/TestSet.java
+++ Alura/Códigos/java-collections/src/br/com/alura/TestSet.java
@@ -2,7 +2,10 @@ package br.com.alura;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class TestSet {
 	public static void main(String[] args) {
@@ -24,5 +27,18 @@ public class TestSet {
 
 		System.out.println(aulas.contains(aula2));
 		System.out.println(aula2.equals(aula3));
+
+		//Forma antiga de percorrer um Set
+		Iterator<Aula> iterator = aulas.iterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+
+		// Guarda a ordem de inserção
+		Set<Aula> aulas2 = new LinkedHashSet<Aula>(Arrays.asList(aula1, aula2, aula3));
+
+		// Não Guarda a ordem de inserção e Necessita que a classe tenha implementado o
+		// Comparable
+		Set<Aula> aulas3 = new TreeSet<Aula>(Arrays.asList(aula1, aula2, aula3));
 	}
 }
