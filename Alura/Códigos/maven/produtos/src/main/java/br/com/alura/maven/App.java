4b01d46fb58485a08a8a456c7e7d1b07eb256140
CharlesLuxinger <charlesluxinger@hotmail.com>
2019-08-09 18:47:54 -0300

    3. Plugins - Relatórios de qualidade de código e cobertura de testes

--- Alura/Códigos/maven/produtos/src/main/java/br/com/alura/maven/App.java
+++ Alura/Códigos/maven/produtos/src/main/java/br/com/alura/maven/App.java
@@ -4,10 +4,10 @@ package br.com.alura.maven;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+
+		Produto bala = new Produto("Juquinha", 0.15);
+		System.out.println("Bala " + bala.getNome() + "preço: " + String.format("%.2f", bala.getPreco()));
+	}
 }
