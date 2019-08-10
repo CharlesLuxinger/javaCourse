97e366ccc9e8e5fde3e31182f68cdbde7c8b597f
Charles Luxinger <charlesluxinger@hotmail.com>
2019-08-10 03:41:15 -0300

    6. Inserindo dependências locais

---
+++ AlgaWoks/testeAlgaWorksJPA/src/main/java/br/com/charles/jpa/teste/App.java
@@ -0,0 +1,13 @@
+package br.com.charles.jpa.teste;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- Alura/Códigos/maven/lojaweb/src/main/java/br/com/charles/maven/lojaweb/ContatoServlet.java
+++ Alura/Códigos/maven/lojaweb/src/main/java/br/com/charles/maven/lojaweb/ContatoServlet.java
@@ -9,10 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.maven.Produto;
+import br.com.caelum.stella.tinytype.CPF;
+
 @WebServlet(urlPatterns = {"/contato"})
 public class ContatoServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Produto p1 = new Produto("Teste", 1.5);
+		new CPF("99999999").isValido();
 		PrintWriter writer = resp.getWriter();
 		writer.println("<html><h2>Fale Conosco!</h2></html>");
 		writer.close();
