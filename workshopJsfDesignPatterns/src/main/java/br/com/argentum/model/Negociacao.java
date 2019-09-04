8c247531e3ff22e93c9b035e77a25c343771c7f6
Charles Luxinger <charlesluxinger@hotmail.com>
2019-09-04 15:17:32 -0300

    1. Initial Commit - Laboratório com Testes, JSF e Design Patterns I

---
+++ workshopJsfDesignPatterns/src/main/java/br/com/argentum/model/Negociacao.java
@@ -0,0 +1,29 @@
+package br.com.argentum.model;
+
+import java.time.LocalDateTime;
+
+public final class Negociacao {
+
+	private final double preco;
+	private final int quantidade;
+	private final LocalDateTime dataHora;
+
+	public Negociacao(double preco, int quantidade, LocalDateTime dataHora) {
+		this.preco = preco;
+		this.quantidade = quantidade;
+		this.dataHora = dataHora;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+
+}
