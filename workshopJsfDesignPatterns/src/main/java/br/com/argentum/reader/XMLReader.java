3ede5c7ec93d6d1c3afe8fd5a308fe9d3e974c7d
Charles Luxinger <charlesluxinger@hotmail.com>
2019-09-05 16:26:53 -0300

    7. Convertido para Maven Project e Escrevendo XML com data em Epoch

---
+++ workshopJsfDesignPatterns/src/main/java/br/com/argentum/reader/XMLReader.java
@@ -0,0 +1,21 @@
+package br.com.argentum.reader;
+
+import java.time.LocalDateTime;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.DomDriver;
+
+import br.com.argentum.model.Negociacao;
+import br.com.argentum.xtream.LocalDateTimeConverterSX;
+
+public class XMLReader {
+	public static void main(String[] args) {
+		Negociacao negociacao = new Negociacao(150, 5, LocalDateTime.now());
+
+		XStream xStream = new XStream(new DomDriver());
+		xStream.registerLocalConverter(Negociacao.class, "dataHora", new LocalDateTimeConverterSX());
+		String xml = xStream.toXML(negociacao);
+
+		System.out.println(xml);
+	}
+}
---
+++ workshopJsfDesignPatterns/src/main/java/br/com/argentum/xtream/LocalDateTimeConverterSX.java
@@ -0,0 +1,55 @@
+package br.com.argentum.xtream;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+
+public class LocalDateTimeConverterSX implements Converter {
+
+	@Override
+	public boolean canConvert(Class type) {
+		return type.isAssignableFrom(LocalDateTime.class);
+	}
+
+	@Override
+	public void marshal(Object source, HierarchicalStreamWriter writer, MarshallingContext context) {
+		LocalDateTime date = (LocalDateTime) source;
+		ZonedDateTime dateZone = date.atZone(ZoneOffset.systemDefault());
+		long milis = dateZone.toInstant().toEpochMilli();
+
+		writer.startNode("time");
+		writer.setValue(String.valueOf(milis));
+		writer.endNode();
+
+		writer.startNode("timezone");
+		writer.setValue(String.valueOf(dateZone.getZone().toString()));
+		writer.endNode();
+	}
+
+	@Override
+	public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+		reader.moveDown();
+		long milis = Long.parseLong(reader.getValue());
+		reader.moveUp();
+
+		reader.moveDown();
+		String dateZone = reader.getValue();
+		reader.moveUp();
+
+		Instant time = Instant.ofEpochMilli(milis);
+
+		ZoneId zone = ZoneId.of(dateZone);
+		ZonedDateTime dateWithZone = ZonedDateTime.ofInstant(time, zone);
+
+		return dateWithZone.toLocalDateTime();
+	}
+
+}
