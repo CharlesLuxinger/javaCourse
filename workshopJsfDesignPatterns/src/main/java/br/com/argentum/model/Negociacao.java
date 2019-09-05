d4687d3d427b7898a5740e5bb04125a9419351ca
Charles Luxinger <charlesluxinger@hotmail.com>
2019-09-05 11:30:06 -0300

    4. Negociação Test

--- workshopJsfDesignPatterns/src/main/java/br/com/argentum/model/Negociacao.java
+++ workshopJsfDesignPatterns/src/main/java/br/com/argentum/model/Negociacao.java
@@ -9,6 +9,15 @@ public final class Negociacao {
 	private final LocalDateTime dataHora;
 
 	public Negociacao(double preco, int quantidade, LocalDateTime dataHora) {
+		if (preco < 0) {
+			throw new IllegalArgumentException("Preço não poder ser negativo.");
+		}
+		if (quantidade <= 0) {
+			throw new IllegalArgumentException("Quantidade não poder ser menor ou igual a zero.");
+		}
+		if (dataHora == null) {
+			throw new IllegalArgumentException("A data deve ser informada.");
+		}
 		this.preco = preco;
 		this.quantidade = quantidade;
 		this.dataHora = dataHora;
---
+++ workshopJsfDesignPatterns/src/test/java/br/com/argentum/model/NegociacaoTest.java
@@ -0,0 +1,34 @@
+package br.com.argentum.model;
+
+import static org.junit.Assert.assertEquals;
+
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+
+public class NegociacaoTest {
+
+	@Test(expected = IllegalArgumentException.class)
+	public void naoDeveCriarNegociaComPrecoNegativo() {
+		new Negociacao(-20, 2, LocalDateTime.now());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void naoDeveCriarNegociaComQuantidadeMenorIgualZero() {
+		new Negociacao(20, 0, LocalDateTime.now());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void naoDeveCriarNegociaSemData() {
+		new Negociacao(20, 1, null);
+	}
+
+	@Test
+	public void testCalculoVolume() {
+		double preco = 2;
+		int quantidade = 2;
+		Negociacao negociacao = new Negociacao(preco, quantidade, LocalDateTime.now());
+
+		assertEquals(preco * quantidade, negociacao.getVolume(), 0.01);
+	}
+}
