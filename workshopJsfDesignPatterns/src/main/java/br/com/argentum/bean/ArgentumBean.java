09f74ddee72e2aa59672109e05bf7ee9bc67c390
Charles Luxinger <charlesluxinger@hotmail.com>
2019-09-06 10:12:03 -0300

    18. Criando paginação e ordenação JSF

--- workshopJsfDesignPatterns/src/main/java/br/com/argentum/bean/ArgentumBean.java
+++ workshopJsfDesignPatterns/src/main/java/br/com/argentum/bean/ArgentumBean.java
@@ -1,17 +1,28 @@
 package br.com.argentum.bean;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.argentum.model.Negociacao;
 import br.com.argentum.webservice.ClientWebService;
 
+@ViewScoped
 @ManagedBean
-public class ArgentumBean {
+public class ArgentumBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<Negociacao> negociacoes;
+
+	public ArgentumBean() {
+		negociacoes = new ClientWebService().getNegociacoes();
+	}
 
 	public List<Negociacao> getNegociacoes() {
-		return new ClientWebService().getNegociacoes();
+		return negociacoes;
 	}
 
 }
